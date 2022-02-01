1e8d2838a4456cea13e56e565521825e9dff2d7c
stigamyr <92983848+stigamyr@users.noreply.github.com>
2022-02-01 12:59:45 +0100

    Revert "hei dete er en komit"
    This reverts commit cdca25fa7e67a38ce5b2c85da46f7fc03ac12c3c.

---
+++ bank.py
@@ -0,0 +1,71 @@
+from tkinter import Event
+import PySimpleGUI as sg
+
+
+
+
+def win1_layout():
+    layout1 = [ 
+
+        [sg.Text("velkommen til SA nord ", size=(20, 5), font=("Helvetica", 25))],
+        [sg.Text("banken som gjør ting enklere", size=(20, 5), font=("Helvetica", 25))],
+         [sg.Text('persjonnummer', size=(10,10)), sg.InputText()],
+         [sg.InputText('', size=(50,10))],
+        [sg.Button("login"),sg.Button("exit")]
+        
+        
+     
+    ]
+
+
+
+
+
+
+
+    
+    return layout1
+
+def login_layout():
+    login_layout =[
+                        ]
+    return login_layout
+
+def vips_layout():
+    vips_layout = [
+        
+    ]
+
+def tab_layout():
+    layout = [
+            [sg.TabGroup(
+                    [[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout), sg.Tab('tredje tab', vips_layout()) ]]
+                )],
+
+            [sg.Button('Read')]
+            
+
+              ]
+    return layout
+
+win1= sg.Window('Everything bagel', default_element_size=(100, )).Layout(win1_layout())
+
+login_win_active = False
+while True:
+    ev1, vals1 = win1.read(timeout=100)
+   
+    if Event in (None, 'Exit'):
+         win1.close()
+         
+         break
+
+    if ev1 == "login":
+        
+        win1.Hide()
+        login_win = sg.Window('Everything bagel', default_element_size=(100, )).Layout(login_layout())
+        login_win_active = True
+    if login_win_active: 
+
+        ev1, vals1 = login_win.read(timeout=100)
+
+    
